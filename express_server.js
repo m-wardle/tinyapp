c6d70bf692646cc1e3b56a7b3db89b2c1d9b61b4
Matthew Wardle <mwardle.22@gmail.com>
2019-10-29 23:58:54 +0000

    Implemented login/logout with proper persistence, using email/password credentials tracked by a unique ID

--- express_server.js
+++ express_server.js
@@ -13,8 +13,31 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const users = {};
+
+const  generateRandomString = function() {
+  let result = '';
+   let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+   let charactersLength = characters.length;
+   for ( let i = 0; i < 6; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+   }
+   return result;
+}
+
+const findEmail = function(str) {
+  let result = []
+  for (let i in users) {
+    if (str === users[i].email) {
+      return users[i].id;
+    }
+  }
+
+  return false;
+};
+
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  res.redirect("/urls")
 });
 
 app.listen(PORT, () => {
@@ -30,34 +53,24 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  let templateVars = { username: req.cookies["username"] }
+  let templateVars = { userInfo: users[req.cookies["user_id"]] }
   res.render("urls_new", templateVars);
 });
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase, username: req.cookies["username"] };
+  let templateVars = { urls: urlDatabase, userInfo: users[req.cookies["user_id"]] };
   res.render("urls_index", templateVars);
 })
 
 app.get("/urls/:shortURL", (req, res) => {
   if (urlDatabase[req.params.shortURL]) {
-    let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"] };
+    let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], userInfo: users[req.cookies["user_id"]] };
     res.render("urls_show", templateVars);
   } else {
     res.statusCode = 404;
     res.send("Short URL not found! Please try again.");
   }
 })
-
-function generateRandomString() {
-  let result = '';
-   let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-   let charactersLength = characters.length;
-   for ( let i = 0; i < 6; i++ ) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-   }
-   return result;
-}
 
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
@@ -88,12 +101,50 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   res.redirect("/urls");
 })
 
+app.get("/register", (req, res) => {
+  let templateVars = { userInfo: users[req.cookies["user_id"]] };
+  res.render("register", templateVars)
+})
+
+app.post("/register", (req, res) => {
+  if (!req.body.email || !req.body.password) {
+    res.statusCode = 400;
+    res.send("Please enter a valid email and password.")
+  } else if (findEmail(req.body.email) === true) {
+    res.statusCode = 400;
+    res.send("An account already exists with that email. Please try again, or log in to your account.")
+  } else {
+    let userID = generateRandomString();
+    users[userID] = {
+    "id": userID,
+    "email": req.body.email,
+    "password": req.body.password
+    };
+    res.cookie("user_id", userID);
+    console.log(users);
+    res.redirect("/urls");
+  }
+})
+
+app.get("/login", (req, res) => {
+  let templateVars = { userInfo: users[req.cookies["user_id"]] };
+  res.render("login", templateVars);
+})
+
 app.post("/login", (req, res) => {
-  res.cookie("username", req.body.username);
-  res.redirect("/urls")
+  if (!findEmail(req.body.email)) {
+    res.statusCode = 403;
+    res.send("Email not found. Please try again.");
+  } else if (req.body.password !== users[findEmail(req.body.email)].password) {
+    res.statusCode = 403;
+    res.send("Incorrect password. Please try again.");
+  } else {
+    res.cookie("user_id", findEmail(req.body.email));
+    res.redirect("/urls")
+  }
 })
 
 app.post("/logout", (req, res) => {
-  res.clearCookie("username");
+  res.clearCookie("user_id");
   res.redirect("back");
 })
