837c4892813ea6af16f9d70349517dd1158dfbb1
Matthew Wardle <mwardle.22@gmail.com>
2019-10-30 21:29:35 +0000

    Made some minor fixes for test requirements, i.e. proper redirects for various cases

--- express_server.js
+++ express_server.js
@@ -3,13 +3,13 @@ const app = express();
 const PORT = 8080;
 const bodyParser = require("body-parser");
 // const cookieParser = require("cookie-parser")
-const cookieSession = require('cookie-session')
-const bcrypt = require("bcrypt")
+const cookieSession = require('cookie-session');
+const bcrypt = require("bcrypt");
 const { findUserByEmail,
-        urlsForUserId,
-        generateRandomString,
-        findUserById
-  } = require("./helpers")
+  urlsForUserId,
+  generateRandomString,
+  findUserById
+} = require("./helpers");
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
@@ -29,7 +29,11 @@ const urlDatabase = {
 const users = {};
 
 app.get("/", (req, res) => {
-  res.redirect("/urls")
+  if (req.session.user_id) {
+    res.redirect("/urls");
+  } else {
+    res.redirect("/login");
+  }
 });
 
 app.listen(PORT, () => {
@@ -46,27 +50,30 @@ app.get("/hello", (req, res) => {
 
 app.get("/urls/new", (req, res) => {
   if (findUserById(req.session.user_id, users)) {
-    let templateVars = { userInfo: users[req.session.user_id] }
+    let templateVars = { userInfo: users[req.session.user_id] };
     res.render("urls_new", templateVars);
   } else {
-    res.redirect("/login")
+    res.redirect("/login");
   }
 });
 
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlsForUserId(req.session.user_id, urlDatabase), userInfo: users[req.session.user_id] };
   res.render("urls_index", templateVars);
-})
+});
 
 app.get("/urls/:shortURL", (req, res) => {
-  if (urlDatabase[req.params.shortURL]) {
-    let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]["longURL"], userInfo: users[req.session.user_id] };
+  if (!findUserById(req.session.user_id, users)) {
+    res.statusCode = 404;
+    res.send("Please <a href='/login'>log in</a> to view specific URLs.");
+  } else if (urlDatabase[req.params.shortURL]) {
+    let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]["longURL"], urlID: urlDatabase[req.params.shortURL]["userID"], userInfo: users[req.session.user_id] };
     res.render("urls_show", templateVars);
   } else {
     res.statusCode = 404;
     res.send("Short URL not found! Please try again.");
   }
-})
+});
 
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
@@ -74,7 +81,7 @@ app.post("/urls", (req, res) => {
   urlDatabase[shortURL] = {
     "longURL": req.body.longURL,
     "userID": req.session.user_id
-   }
+  };
   console.log(urlDatabase);
   res.redirect(`/urls/${shortURL}`);
   res.statusCode = 303;
@@ -92,64 +99,81 @@ app.get("/u/:shortURL", (req, res) => {
 });
 
 app.post("/urls/:shortURL", (req, res) => {
+  if (!findUserById(req.session.user_id, users)) {
+    res.statusCode = 404;
+    res.send("Please <a href='/login'>log in</a> to create and edit URLs.");
+  } else if (urlDatabase[req.params.shortURL].userID !== req.session.user_id) {
+    res.statusCode = 404;
+    res.send("You don't have the proper permissions to edit that.\nEither <a href='/login'>log in</a> to the correct account, or <a href='/urls'>go back.</a>\n");
+  }
   urlDatabase[req.params.shortURL] = req.body.longURL;
-  res.redirect('/urls')
-})
+  res.redirect('/urls');
+});
 
 app.post("/urls/:shortURL/delete", (req, res) => {
-  if (urlDatabase[req.params.shortURL].userID === req.session.user_id){
+  if (urlDatabase[req.params.shortURL].userID === req.session.user_id) {
     delete urlDatabase[req.params.shortURL];
     res.redirect("/urls");
   } else {
-    res.send("You don't have the proper permissions to do that!\n😡 Don't 😡 delete 😡 other 😡 people's 😡 links 😡\n")
+    res.send("You don't have the proper permissions to do that!\n😡 Don't 😡 delete 😡 other 😡 people's 😡 links 😡\n");
   }
-})
+});
 
 app.get("/register", (req, res) => {
-  let templateVars = { userInfo: users[req.session.user_id] };
-  res.render("register", templateVars)
-})
+  if (req.session.user_id) {
+    res.statusCode = 302;
+    res.redirect("/urls");
+  } else {
+    let templateVars = { userInfo: users[req.session.user_id] };
+    res.render("register", templateVars);
+  }
+});
 
 app.post("/register", (req, res) => {
   if (!req.body.email || !req.body.password) {
     res.statusCode = 400;
-    res.send("Please enter a valid email and password.")
+    res.send("Please enter a valid email and password.");
   } else if (findUserByEmail(req.body.email, users)) {
     res.statusCode = 400;
-    res.send("An account already exists with that email. Please try again, or log in to your account.")
+    res.send("An account already exists with that email. Please try again, or log in to your account.");
   } else {
     let userID = generateRandomString();
     users[userID] = {
-    "id": userID,
-    "email": req.body.email,
-    "password": bcrypt.hashSync(req.body.password, 10)
+      "id": userID,
+      "email": req.body.email,
+      "password": bcrypt.hashSync(req.body.password, 10)
     };
     // res.cookie("user_id", userID);
     req.session.user_id = userID;
     console.log(users);
     res.redirect("/urls");
   }
-})
+});
 
 app.get("/login", (req, res) => {
-  let templateVars = { userInfo: users[req.session.user_id] };
-  res.render("login", templateVars);
-})
+  if (req.session.user_id) {
+    res.statusCode = 302;
+    res.redirect("/urls");
+  } else {
+    let templateVars = { userInfo: users[req.session.user_id] };
+    res.render("login", templateVars);
+  }
+});
 
 app.post("/login", (req, res) => {
   if (!findUserByEmail(req.body.email, users)) {
     res.statusCode = 403;
-    res.send("Email not found. Please try again.");
+    res.send("Email not found. Please <a href='/login'>try again.</a>");
   } else if (!bcrypt.compareSync(req.body.password, users[findUserByEmail(req.body.email, users)].password)) {
     res.statusCode = 403;
-    res.send("Incorrect password. Please try again.");
+    res.send("Incorrect password. Please <a href='/login'>try again.</a>");
   } else {
     req.session.user_id = findUserByEmail(req.body.email, users);
-    res.redirect("/urls")
+    res.redirect("/urls");
   }
-})
+});
 
 app.post("/logout", (req, res) => {
   req.session = null;
   res.redirect("back");
-})
+});
