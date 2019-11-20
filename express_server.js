250d91fe23dae32731429a7705ea4d93eadfa901
Matthew Wardle <50600031+m-wardle@users.noreply.github.com>
2019-11-20 17:57:46 -0500

    Update express_server.js

--- express_server.js
+++ express_server.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const app = express();
-const PORT = 8080;
+const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const bcrypt = require('bcrypt');
